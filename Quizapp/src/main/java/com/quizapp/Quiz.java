4eea3d284d80a44bffda7466370d7bf07e06c81f
Mujahid-15 <mujahidasayyed15@gmail.com>
2024-08-08 16:45:10 +0530

    Add files via upload

---
+++ Quizapp/src/main/java/com/quizapp/Quiz.java
@@ -0,0 +1,29 @@
+package com.quizapp;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quiz {
+    private String title;
+    private List<Question> questions;
+
+    public Quiz(String title) {
+        this.title = title;
+        this.questions = new ArrayList<>();
+    }
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+}
+
